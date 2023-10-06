1866515b561d2a4324d1ebfff0d2e034981d8702
mohammadzam <mbHPvf4UiE982nV>
2023-10-06 22:03:47 +0330

    fix some prefix in some routes

--- Modules/Employer/Routes/web.php
+++ Modules/Employer/Routes/web.php
@@ -41,8 +41,13 @@ Route::prefix('panel/employer/management-affairs/switch-account')->controller('E
     ->middleware(['auth:employer','employerProfileCompleted','enabledEmployer','IsEmployerVerifyEmail'])->group(function() {
     Route::post('/', 'switchToWorker')->name('employer.switch.account.to.worker');
     Route::get('history','history')->name('employer.show.switching.account.history');
-    Route::get('employer-to-worker-with-transfer-wallet-balance','showSwitchToWorkerAndTransferWalletBalanceForm')->name('employer.show.switch.account.to.worker.with.transfer.wallet.balance.form');
-    Route::post('switch-to-worker-and-transfer-balance','switchToWorkerAndTransferWalletBalance')->name('employer.switch.account.to.worker.with.transfer.wallet.balance');
 
+});
+
+Route::prefix('panel/employer/financial-affairs/switch-account')->controller('Employer\SwitchingAccountController')
+    ->middleware(['auth:employer','employerProfileCompleted','enabledEmployer','IsEmployerVerifyEmail'])->group(function() {
+        Route::get('employer-to-worker-with-transfer-wallet-balance','showSwitchToWorkerAndTransferWalletBalanceForm')->name('employer.show.switch.account.to.worker.with.transfer.wallet.balance.form');
+
+        Route::post('switch-to-worker-and-transfer-balance','switchToWorkerAndTransferWalletBalance')->name('employer.switch.account.to.worker.with.transfer.wallet.balance');
 });
 
--- Modules/Worker/Routes/web.php
+++ Modules/Worker/Routes/web.php
@@ -19,24 +19,27 @@ Route::prefix('panel/worker')->controller('Dashboard\WorkerDashboardController')
     Route::post('log-out', 'logout')->name('worker.logout');
 });
 
-Route::prefix('panel/worker/my-profile')->controller('Worker\WorkerProfileController')->middleware('auth:worker')->group(function() {
+Route::prefix('panel/worker/my-profile')->controller('Worker\WorkerProfileController')->middleware('auth:worker')->group(function () {
     Route::get('/', 'showMyProfile')->name('worker.show.my.profile');
-    Route::get('edit-my-profile','showUpdateMyProfileForm')->name('worker.show.edit.my.profile.form');
+    Route::get('edit-my-profile', 'showUpdateMyProfileForm')->name('worker.show.edit.my.profile.form');
     Route::post('update-my-profile', 'updateMyProfile')->name('worker.update.my.profile');
     Route::post('fetch-cities', 'fetchCity')->name('worker.fetch.cities.when.update.profile');
 });
 
-Route::prefix('panel/worker/verify-account')->middleware('auth:worker')->controller('Worker\VerifyAccountController')->group(function() {
+Route::prefix('panel/worker/verify-account')->middleware('auth:worker')->controller('Worker\VerifyAccountController')->group(function () {
     Route::get('send-verify-email', 'SendEmailVerification')->name('worker.send.email.verify');
     Route::get('verify-email-account/{token}/{worker_number}', 'verifyAccount')->name('verify.worker.account');
     Route::get('send-sms-verification', 'SendSMSVerification')->name('worker.send.sms.verification');
 
 });
 Route::prefix('panel/worker/management-affairs/switchAccount')->controller('Worker\SwitchingAccountController')
-    ->middleware(['auth:worker','workerProfileCompleted','enabledWorker','IsWorkerVerifyEmail'])->group(function() {
-    Route::post('/', 'switchToEmployer')->name('worker.switch.account.to.employer');
-    Route::get('history','history')->name('worker.show.switching.account.history');
-    Route::get('worker-to-employer-with-transfer-wallet-balance','showSwitchToEmployerAndTransferWalletBalanceForm')->name('worker.show.switch.account.to.employer.with.transfer.wallet.balance.form');
-    Route::post('switch-to-employer-and-transfer-balance','switchToEmployerAndTransferWalletBalance')->name('worker.switch.account.to.employer.with.transfer.wallet.balance');
+    ->middleware(['auth:worker', 'workerProfileCompleted', 'enabledWorker', 'IsWorkerVerifyEmail'])->group(function () {
+        Route::post('/', 'switchToEmployer')->name('worker.switch.account.to.employer');
+        Route::get('history', 'history')->name('worker.show.switching.account.history');
+    });
+Route::prefix('panel/worker/financial-affairs/switchAccount')->controller('Worker\SwitchingAccountController')
+    ->middleware(['auth:worker', 'workerProfileCompleted', 'enabledWorker', 'IsWorkerVerifyEmail'])->group(function () {
+        Route::get('worker-to-employer-with-transfer-wallet-balance', 'showSwitchToEmployerAndTransferWalletBalanceForm')->name('worker.show.switch.account.to.employer.with.transfer.wallet.balance.form');
+        Route::post('switch-to-employer-and-transfer-balance', 'switchToEmployerAndTransferWalletBalance')->name('worker.switch.account.to.employer.with.transfer.wallet.balance');
 
-});
+    });
