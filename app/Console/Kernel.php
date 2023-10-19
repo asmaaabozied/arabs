2cc87d11d8b54a924d758fe76dc3a51398ab55a3
mohammadzam <mbHPvf4UiE982nV>
2023-10-19 22:29:35 +0330

    add some job into kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Modules\Admin\Jobs\AutoEnableEmployerStatus;
+use Modules\Admin\Jobs\AutoEnableWorkerStatus;
+use Modules\Admin\Jobs\AutoUpdateCurrencyRate;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new AutoUpdateCurrencyRate())->daily();
+        $schedule->job(new AutoEnableWorkerStatus())->daily();
+        $schedule->job(new AutoEnableEmployerStatus())->daily();
     }
 
     /**
